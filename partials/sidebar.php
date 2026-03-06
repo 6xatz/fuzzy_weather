4668081e03fa5f9b6e4bde475fdc7b5ca705ced2
6xatz <fluthfisyx@gmail.com>
2026-03-07 06:36:40 +0700

    Remove all files from repository

--- partials/sidebar.php
+++
@@ -1,9 +0,0 @@
-<aside class="sidebar">
-	<h2 class="brand">Fuzzy Weather</h2>
-	<nav class="nav">
-		<a href="index.php?page=dashboard" class="<?php echo (($_GET['page']??'dashboard')==='dashboard')?'active':''; ?>">Dashboard</a>
-		<a href="index.php?page=history" class="<?php echo (($_GET['page']??'dashboard')==='history')?'active':''; ?>">History</a>
-		<div style="height:12px"></div>
-		<a href="logout.php">Keluar</a>
-	</nav>
-</aside>
